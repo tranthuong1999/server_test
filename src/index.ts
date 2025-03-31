b23507940f3d3f0defa11dc1c8b1327a42d73fc9
MartinTran <macpro2015@MartinTran.local>
2025-03-31 21:48:47 +0700

    add /

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,9 @@ app.use(cors({
 mongoose.connect(process.env.DB_TEST as string, {});
 
 app.use("/api/employees", employeeRoutes);
+app.get("/", async (req, res) => {
+    res.json({ message: "Hello Employeer" });
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
